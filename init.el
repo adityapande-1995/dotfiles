d3df7e45d55afe1265d009b8fd6f4b64038f5616
Aditya Pande <aditya050995@gmail.com>
2021-01-30 00:49:56 -0700

    Update init.el

--- init.el
+++ init.el
@@ -1,9 +1,15 @@
-;;                                   
-;;  mmm   mmmmm   mmm    mmm    mmm  
-;; #"  #  # # #  "   #  #"  "  #   " 
-;; #""""  # # #  m"""#  #       """m 
-;; "#mm"  # # #  "mm"#  "#mm"  "mmm" 
-;;                                   
+;; 
+;;  ______________________
+;; / ░█▀▀░█▄█░█▀█░█▀▀░█▀▀ \
+;; | ░█▀▀░█░█░█▀█░█░░░▀▀█ |
+;; \ ░▀▀▀░▀░▀░▀░▀░▀▀▀░▀▀▀ /
+;;  ----------------------
+;;         \   ^__^
+;;          \  (oo)\_______
+;;             (__)\       )\/\
+;;                 ||----w |
+;;                 ||     ||
+;; 
 
 ;; ------  Basic  evil mode ------
 ;; Set up package.el to work with MELPA
@@ -121,6 +127,43 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
 
+;; --------------- lsp-mode ------------------
+;; from https://emacs-lsp.github.io/lsp-mode/tutorials/CPP-guide/
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
+    projectile hydra flycheck company avy which-key helm-xref dap-mode))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
+
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'cpp-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1 ;; clangd is fast
+      ;; be more ide-ish
+      lsp-headerline-breadcrumb-enable t)
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
+;; Turn on company mode for prog and cmake derived modes
+(add-hook 'prog-mode-hook 'company-mode)
+(add-hook 'cmake-mode-hook 'company-mode)
 
 ;; --------- Random keybinds and specifics ---------- 
 ;; linum mode spacing
